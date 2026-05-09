a2b7f1509cbd62a34bb01b85d6d581406bea4f60
tomasmariotti <tomasmariotti2@gmail.com>
2026-05-08 21:23:34 -0300

    Fix favicon reference to use correct filename

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -82,7 +82,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:site", content: "@UltraFitness" },
     ],
     links: [
-      { rel: "icon", type: "image/x-icon", href: "/ultrafitnes.ico" },
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
